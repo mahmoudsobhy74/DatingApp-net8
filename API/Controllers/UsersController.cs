512983656ee5e0919a8161512d49d5ebbe417b21
Mahmoud.Sobhy <Mahmoud.Sobhy@techprocess.net>
2024-11-02 17:16:40 +0200

    End of section 5

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
-[Authorize]
+
 public class UsersController(DataContext context) : BaseApiController // C# 12 Primary constructor feature
 {
     [AllowAnonymous]
