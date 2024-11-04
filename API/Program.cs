3f445a43c8e537827092031b6aaecfbabfdcd44a
Mahmoud.Sobhy <Mahmoud.Sobhy@techprocess.net>
2024-11-04 16:57:56 +0200

    End of section 7

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Extension;
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,10 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddApplicationServics(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
 
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
     .WithOrigins("http://localhost:4200","https://localhost:4200"));
 
